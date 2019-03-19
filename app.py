9fa3384c87ea24a9d81654f257292c96b13e71fb
Yussuf Shaikh <yussuf@us.ibm.com>
2019-03-19 10:58:22 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+import os
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/hello/')
+def hello_world():
+    return 'Hello World!\n'
+
+@app.route('/hello/<username>')
+def hello_user(username):
+    return 'Why Hello %s!\n' % username
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
