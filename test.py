a13a3054c2ecdf1d95d3271550e26860c73bf691
Yussuf Shaikh <yussuf@us.ibm.com>
2019-03-19 11:37:17 +0530

    Update test.py

--- test.py
+++ test.py
@@ -22,6 +22,7 @@ class TestHello(unittest.TestCase):
         name = 'User'
         rv = self.app.get('/hello/{0}'.format(name))
         self.assertEqual(rv.status, '200 OK')
+        print(rv.data)
         self.assertIn(bytearray("{0}".format(name), 'utf-8'), rv.data)
 
 if __name__ == '__main__':
